d5d1d4f143f1829a8eac34ab5f09f1cc3cde0243
iBug <git@ibugone.com>
2019-07-17 13:33:30 +0800

    Update Docker entrypoint for architecture rebuilding

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,44 +4,8 @@
 
 cd "$(dirname "$0")"
 
-check_env() {
-  : ${SOURCE:=_site/}
-  for key in HOST USER PASS TARGET; do
-    if [ -z "${!key}" ]; then
-      echo "Missing variable ${key}!" >&2
-      exit 1
-    fi
-  done
-}
+git remote update
+git reset --hard origin/master
+bundle check || bundle update
 
-update() {
-  git remote update
-  git reset --hard origin/master
-  bundle check || bundle install
-}
-
-build() {
-  # "build" should abort whole process at failure
-  JEKYLL_ENV=production bundle exec jekyll build || exit
-}
-
-deploy() {
-  lftp -c "
-  set ftp:list-options -a
-  open $HOST
-  user $USER $PASS
-  mirror --reverse --delete --verbose $SOURCE $TARGET
-  bye
-  "
-}
-
-cleanup() {
-  # Keep the container clean
-  rm -rf _site
-}
-
-check_env
-update
-build || exit
-deploy
-cleanup
+JEKYLL_ENV=production bundle exec jekyll build --destination /srv/site || exit
